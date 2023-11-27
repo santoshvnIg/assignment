d79b7591e0c79513ea53f0f0483b0cac52f44dc8
santoshvnIg <santosh.veranekar@impactguru.com>
2023-11-27 17:47:46 +0530

    done with Ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import Head from "./assesmentComponents/Heads.jsx";
+import Careers from "./assesmentComponents/Careers.jsx";
+import Communitys from "./assesmentComponents/Communitys.jsx";
+import Jobs from "./assesmentComponents/Jobs.jsx";
+import Speakers from "./assesmentComponents/Speakers.jsx";
+import ProgramDetail from "./assesmentComponents/ProgamDetails.jsx";
+import Updates from "./assesmentComponents/Updates.jsx";
+import Paths from "./assesmentComponents/Paths.jsx";
+import Blogss from "./assesmentComponents/Blogs.jsx";
+import Footers from "./assesmentComponents/Footers.jsx";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+      <Head />
+      <Careers />
+      <Communitys />
+      <Jobs />
+      <Speakers />
+      <ProgramDetail />
+      <Updates />
+      <Paths />
+      <Blogss/>
+      <Footers />
+    </div>
     </div>
   );
 }
---
+++ src/assesmentComponents/Blogs.jsx
@@ -0,0 +1,127 @@
+import React from "react";
+import { Card } from "antd";
+import { Col, Row } from "antd";
+
+const Blogss = () => {
+  return (
+    <div className="Blogss">
+      <div className="Blogss_main bg-slate-200">
+        <h1 className="font-family text-3xl font-extrabold text-black text-center py-14">
+          {" "}
+          From the Blogss{" "}
+        </h1>
+        <Row gutter={[50, 20]} className="px-10">
+          <Col className="gutter-row" span={8}>
+            <Card className="rounded-3xl">
+              <div className="">
+                <img
+                  className="rounded-3xl"
+                  src="https://19545538.fs1.hubspotusercontent-na1.net/hub/19545538/hubfs/professional-staffing-agencies-980x653.webp?width=390&height=230&name=professional-staffing-agencies-980x653.webp"
+                  alt=""
+                />
+              </div>
+
+              <h1 className="font-family font-bold text-xl py-5">
+                {" "}
+                What Does a Professional Staffing Agency Do For Jobs Seekers?{" "}
+              </h1>
+
+              <p className="font-family text-sm font-medium pb-16">
+                {" "}
+                Successful{" "}
+                <a
+                  className="text-orange-600 hover:underline hover:text-orange-600 cursor-pointer"
+                  href=""
+                >
+                  {" "}
+                  professional staffing agencies{" "}
+                </a>{" "}
+                can save both employees and employers time and money. Wherever
+                you live, there are professional staffing agencies ready to help
+                you find your...{" "}
+              </p>
+
+              <button className="font-family font-medium bg-black py-2 px-7 rounded-lg text-white hover:bg-orange-600 hover:text-white transition duration-500 shadow-black shadow-sm">
+                {" "}
+                Read More{" "}
+              </button>
+            </Card>
+          </Col>
+
+          <Col className="gutter-row" span={8}>
+            <Card className="rounded-3xl">
+              <div className="">
+                <img
+                  className="rounded-3xl"
+                  src="https://19545538.fs1.hubspotusercontent-na1.net/hub/19545538/hubfs/Local-Staffing-Agency-1024x683-1-1024x675.webp?width=390&height=230&name=Local-Staffing-Agency-1024x683-1-1024x675.webp"
+                  alt=""
+                />
+              </div>
+
+              <h1 className="font-family font-bold text-xl py-5">
+                {" "}
+                Choosing the Right Local Staffing Agency to Find a Jobs{" "}
+              </h1>
+
+              <p className="font-family text-sm font-medium pb-5">
+                {" "}
+                Choosing the Right Staffing Agency as a Jobs Seeker{" "}
+              </p>
+
+              <p className="font-family text-sm font-medium pb-6">
+                {" "}
+                Working with a staffing agency is often a great way to find a
+                Jobs, especially during difficult economic conditions. However,
+                not...{" "}
+              </p>
+
+              <button className="font-family font-medium bg-black py-2 px-7 rounded-lg text-white hover:bg-orange-600 hover:text-white transition duration-500 shadow-black shadow-sm">
+                {" "}
+                Read More{" "}
+              </button>
+            </Card>
+          </Col>
+
+          <Col className="gutter-row" span={8}>
+            <Card className="rounded-3xl">
+              <div className="">
+                <img
+                  className="rounded-3xl w-96 h-56"
+                  src="https://19545538.fs1.hubspotusercontent-na1.net/hub/19545538/hubfs/staffing%20agency.jpg?width=390&height=230&name=staffing%20agency.jpg"
+                  alt=""
+                />
+              </div>
+
+              <h1 className="font-family font-bold text-xl py-5">
+                {" "}
+                Find Your Next Jobs Through LG Resources Staffing Agency{" "}
+              </h1>
+
+              <p className="font-family text-sm font-medium pb-16">
+                {" "}
+                Most of us need to work to pay the bills. Unfortunately, it’s
+                not always easy to find work when you need it, especially if
+                you’re looking for a specific type of Jobs.{" "}
+              </p>
+
+              <button className="font-family font-medium bg-black py-2 px-7 rounded-lg text-white hover:bg-orange-600 hover:text-white transition duration-500 shadow-black shadow-sm">
+                {" "}
+                Read More{" "}
+              </button>
+            </Card>
+          </Col>
+        </Row>
+        <div className="flex justify-center py-14">
+          <button className="font-family font-medium bg-transparent py-4 px-5 rounded-xl text-black border border-black hover:bg-orange-600 hover:text-white transition duration-700 shadow-black shadow-sm">
+            <a href="https://Jobss.lgresources.com/Blogss" target="_blank">
+              {" "}
+              View More Articles{" "}
+            </a>{" "}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Blogss;
